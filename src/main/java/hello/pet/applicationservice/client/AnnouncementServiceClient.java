798b65b403fdab6e82f8f1396cd9a663144cc9f8
xeimun <hinote444@naver.com>
2025-10-13 17:20:53 +0900

    :recycle: refactor: FeignClient 속성 수정

--- src/main/java/hello/pet/applicationservice/client/AnnouncementServiceClient.java
+++ src/main/java/hello/pet/applicationservice/client/AnnouncementServiceClient.java
@@ -6,12 +6,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "announcement-service")
+@FeignClient(
+        name = "announcement-service",
+        url = "http://announcement-service:8084",
+        path = "/v1/announcements"
+)
 public interface AnnouncementServiceClient {
 
-    @GetMapping("/v1/announcements/{id}")
-    AnnouncementResponse getAnnouncementById(@PathVariable Long id);
+    @GetMapping("/{id}")
+    AnnouncementResponse getAnnouncementById(@PathVariable("id") Long id);
 
-    @PatchMapping("/v1/announcements/{id}/complete")
-    void completeAnnouncement(@PathVariable Long id);
+    @PatchMapping("/{id}/complete")
+    void completeAnnouncement(@PathVariable("id") Long id);
 }
